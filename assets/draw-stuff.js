18a02e31bdf679d02ddf258af1bfc83b87eaf89d
John S <tinco7@aol.com>
2019-09-21 23:00:40 -0700

    I think it's working!

--- assets/draw-stuff.js
+++ assets/draw-stuff.js
@@ -3,9 +3,6 @@
 // ------------------------------------------------------------
 
 // FUN. Draw filled rect.
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
 function Create2DArray(rows) {
   var arr = [];
 
@@ -17,16 +14,18 @@ function Create2DArray(rows) {
 }
 async function draw_ant( ctx , posx, posy, color)
 {
-	ctx.save( );
+	//ctx.save( );
 	if (color == 0) fill = 'black';
 	if (color == 1) fill = 'red';
 	if (color == 2) fill = 'yellow';
 	if (color == 3) fill = 'blue';
+  ctx.beginPath();
     ctx.fillStyle = fill;
-    ctx.rect(posx*10, posy*10, 10, 10);
+
+    ctx.rect(posx*10 + 1, posy*10 + 2, 7, 7);
     ctx.stroke( );
     ctx.fill( );
-    ctx.restore( );
+  //  ctx.restore( );
 }
 
 function goleft(ant, ctx, color){
@@ -46,7 +45,7 @@ function goleft(ant, ctx, color){
 		ant.dir = 2;
 		ant.y = ant.y + 1;
 	}
-	
+
 	draw_ant(ctx, ant.x, ant.y, color);
 }
 function goright(ant, ctx, color){
@@ -77,21 +76,21 @@ function draw_grid( rctx, rminor, rmajor, rstroke, rfill )
     rctx.fillStyle = rfill;
     let width = rctx.canvas.width;
     let height = rctx.canvas.height;
-    for ( var ix = 5; ix < width; ix += rminor )
+    for ( var ix = 0; ix < width; ix += rminor )
     {
         rctx.beginPath( );
         rctx.moveTo( ix, 0 );
         rctx.lineTo( ix, height );
-        rctx.lineWidth = ( ix % rmajor == 0 ) ? 0.5 : 0.25;
+        rctx.lineWidth = ( ix % rmajor == 0 ) ? .5 : .5;
         rctx.stroke( );
         if ( ix % rmajor == 0 ) { rctx.fillText( ix, ix, 10 ); }
     }
-    for ( var iy = 5; iy < height; iy += rminor )
+    for ( var iy = 0; iy < height; iy += rminor )
     {
         rctx.beginPath( );
         rctx.moveTo( 0, iy );
         rctx.lineTo( width, iy );
-        rctx.lineWidth = ( iy % rmajor == 0 ) ? 0.5 : 0.25;
+        rctx.lineWidth = ( iy % rmajor == 0 ) ? .5 : .5;
         rctx.stroke( );
         if ( iy % rmajor == 0 ) {rctx.fillText( iy, 0, iy + 10 );}
     }
